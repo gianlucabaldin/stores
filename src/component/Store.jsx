a2c630be41e7e5403a64de205246fac46cd41062
Gianluca Baldin <gbaldin@intesys.it>
2020-05-15 19:01:47 +0200

    styling Store

--- src/component/Store.jsx
+++ src/component/Store.jsx
@@ -1,13 +1,6 @@
 import React from "react";
 
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  Typography,
-  makeStyles,
-} from "@material-ui/core";
+import { Button, Card, Typography, makeStyles, Box } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
@@ -15,8 +8,11 @@ const useStyles = makeStyles({
     display: "flex",
     justifyContent: "space-between",
     marginBottom: 20,
+    height: 50,
+    alignItems: "center",
     border: "1px solid lightgrey",
     boxShadow: "none",
+    padding: "10px 20px",
   },
   title: {
     fontSize: 16,
@@ -26,6 +22,9 @@ const useStyles = makeStyles({
     backgroundColor: "#7ED321",
     color: "white",
   },
+  distanceBox: {
+    minWidth: 150,
+  },
 });
 
 export const Store = ({ name, distance }) => {
@@ -33,19 +32,20 @@ export const Store = ({ name, distance }) => {
   return (
     <div>
       <Card className={classes.root}>
-        <CardContent>
-          <Typography className={classes.title} gutterBottom>
-            {name}
+        <Typography className={classes.title} gutterBottom>
+          {name}
+        </Typography>
+        <Box className={classes.distanceBox}>
+          <Typography component="span" variant="body2" color="textSecondary">
+            Distanza:{" "}
           </Typography>
-          <Typography variant="body2" color="textSecondary">
-            Distanza {distance} km
+          <Typography component="span" className={classes.title} gutterBottom>
+            {distance} km
           </Typography>
-        </CardContent>
-        <CardActions style={{ padding: 20 }}>
-          <Button size="medium" variant="contained" className={classes.button}>
-            Seleziona
-          </Button>
-        </CardActions>
+        </Box>
+        <Button size="medium" variant="contained" className={classes.button}>
+          Seleziona
+        </Button>
       </Card>
     </div>
   );
