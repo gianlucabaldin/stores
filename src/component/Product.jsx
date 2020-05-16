e0815ab6877d718504da5891258e4401171f186c
Gianluca Baldin <gbaldin@intesys.it>
2020-05-16 12:43:03 +0200

    pass productName from Product to Store

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -48,6 +48,7 @@ export const Product = ({ id, name, description, handleClick }) => {
               pathname: "/store-list",
               state: {
                 productId: id,
+                productName: name,
               },
             }}
           >
