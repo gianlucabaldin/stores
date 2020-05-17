47fda3614ac0292e55f57adbfb87638050491a64
Gianluca Baldin <gbaldin@intesys.it>
2020-05-17 22:49:41 +0200

    check on route /store-list if no step1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import ProductList from "./component/ProductList";
 import StoreList from "./component/StoreList";
 
@@ -9,9 +14,26 @@ export default () => (
       <Route exact path="/">
         <ProductList />
       </Route>
-      <Route path="/store-list">
-        <StoreList />
+      <Route path="/store-list" render={(props) => checkProduct(props)} />
+      {/* Fallback for not found route */}
+      <Route path="/">
+        <ProductList />
       </Route>
     </Switch>
   </Router>
 );
+
+/**
+ * Prevents going to /store-list if the url is typed as url by user without
+ * passing through the step 1 (product selection), in case redirects to homepage
+ * @param {object} location router's location object contains the state
+ */
+const checkProduct = ({ location }) => {
+  const { state } = location;
+  debugger;
+  return state !== undefined && !!state.productId && !!state.productName ? (
+    <StoreList />
+  ) : (
+    <Redirect to="/" />
+  );
+};
